50eb6c29229b3b4cf59b25f68cfd618aec31bafe
Bantayehu F <bantayehuf@gmail.com>
2023-08-06 12:51:05 +0300

    Breaked down sidebar and navbar as components

--- resources/views/components/layout/sub/sidebar-collapse-item.blade.php
+++ resources/views/components/layout/sub/sidebar-collapse-item.blade.php
@@ -2,7 +2,7 @@
 
 <li>
     <a href="{{ _($route) }}"
-        class="flex items-center p-2 pl-7 w-full text-base font-medium text-gray-900 rounded-lg transition duration-75 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700">
+        class="flex items-center p-2 pl-9 w-full text-base font-medium text-gray-900 rounded-lg transition duration-75 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700">
         {{ _($label) }}
     </a>
 </li>
